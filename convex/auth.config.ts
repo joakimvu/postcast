a0080f55390ebfe571e87562ed7ee9a1f9f32d48
Joakim Vu <joakimvu@icloud.com>
2024-10-19 07:17:56 +0200

    add auth and db

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+// https://square-lionfish-36.clerk.accounts.dev
+const authConfig = {
+  providers: [
+    {
+      domain: "https://square-lionfish-36.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
