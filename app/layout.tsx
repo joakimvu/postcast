1ab4946b79d3139a52bda6fc1dee4ba45574f0d0
Joakim Vu <joakimvu@icloud.com>
2024-10-19 07:17:29 +0200

    add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
 
 export const metadata: Metadata = {
   title: "Podcast",
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <ConvexClerkProvider>
+        <body>{children}</body>
+      </ConvexClerkProvider>
     </html>
   );
 }
